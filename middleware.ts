b121599a3811570daee351e5b6eed81981311ff1
Truong Nguyen <mnguyen@evenica.com>
2024-10-11 14:30:39 -0400

    init

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,11 @@
-import { find } from 'geo-tz'
 import { DateTime } from 'luxon'
 import { type NextRequest } from 'next/server'
 
 export default function middleware(request: NextRequest) {
   const latitude = parseFloat(request.geo?.latitude ?? '0')
   const longitude = parseFloat(request.geo?.longitude ?? '0')
-  const [tz] = find(latitude, longitude)
-  const time = DateTime.now().setZone(tz).toISOTime({ suppressMilliseconds: true })
+  // const [tz] = find(latitude, longitude)
+  const time = DateTime.now().setZone('America/New_York').toISOTime({ suppressMilliseconds: true })
   console.log(time)
 }
 
