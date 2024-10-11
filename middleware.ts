b2cc6539a48ecd5b1bf9fd89fb5433c72d39db45
Truong Nguyen <mnguyen@evenica.com>
2024-10-11 14:05:51 -0400

    init

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,10 @@
-import { type NextRequest, NextResponse } from 'next/server'
+import { type NextRequest } from 'next/server'
+import { getSunset } from 'sunrise-sunset-js'
 
 export default function middleware(request: NextRequest) {
-  const { country } = request.geo ?? {}
-  console.log(request.geo)
-  const response = NextResponse.next()
-  response.headers.set('User-Country', country ?? 'Canada')
-  return response
+  const { latitude = '0', longitude = '0' } = request.geo ?? {}
+  const sunset = getSunset(parseFloat(latitude), parseFloat(longitude))
+  console.log(sunset)
 }
 
 export const config = {
