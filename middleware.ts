4263c394b1a9b35f1a76e7e1e84a494083afeb1e
Truong Nguyen <mnguyen@evenica.com>
2024-10-11 15:26:01 -0400

    init

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,13 @@
 import { type NextRequest } from 'next/server'
 
-export default function middleware(_request: NextRequest) {
-  // const { city = 'Toronto', country = 'CA' } = request.geo ?? {}
-  // const tz = findTimezoneByCityName(city) ?? ''
-  console.log(process.env.TIMEZONEDB)
+export default async function middleware(request: NextRequest) {
+  const { latitude = '43.351330', longitude = '-79.799380' } = request.geo ?? {}
+  const response = await fetch(
+    `https://api.timezonedb.com/v2.1/get-time-zone?key=${process.env.TIMEZONEDB}&format=json&by=position&lat=${parseFloat(latitude)}&lng=${parseFloat(longitude)}`,
+  )
+  const { formatted } = (await response.json()) as { formatted: string }
+  const currentHour = new Date(formatted).getHours()
+  console.log('current hour: ', currentHour)
 }
 
 export const config = {
