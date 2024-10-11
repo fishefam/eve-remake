d2397ac66026d22477778ea67297b04af4a12574
Truong Nguyen <mnguyen@evenica.com>
2024-10-11 15:34:09 -0400

    init

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ export default async function middleware(request: NextRequest) {
   const response = await fetch(
     `https://api.timezonedb.com/v2.1/get-time-zone?key=${process.env.TIMEZONEDB}&format=json&by=position&lat=${parseFloat(latitude)}&lng=${parseFloat(longitude)}`,
   )
-  const { formatted } = JSON.parse(await response.text()) as { formatted: string }
+  const { formatted } = (await response.text()) as unknown as { formatted: string }
   const currentHour = new Date(formatted).getHours()
   console.log('current hour: ', currentHour)
 }
