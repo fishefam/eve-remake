a6a70cbf087bcbc26a74cdb1f208e1dc9e17db59
Truong Nguyen <mnguyen@evenica.com>
2024-10-11 14:57:16 -0400

    init

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,10 @@
-import { lookupViaCity } from 'city-timezones'
 import { type NextRequest } from 'next/server'
+import { findTimezoneByCityName } from 'node-location-timezone'
 
 export default function middleware(request: NextRequest) {
   const { city = 'Toronto', country = 'CA' } = request.geo ?? {}
-  const cityData = lookupViaCity(city) ?? {}
-  const timezones = cityData.map(({ timezone }) => timezone)
-  console.log(country, city, timezones)
+  const tz = findTimezoneByCityName(city) ?? ''
+  console.log(country, city, tz)
   // const time = DateTime.now().setZone('America/New_York').toISOTime({ suppressMilliseconds: true })
   // console.log(time)
 }
