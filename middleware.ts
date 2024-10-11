22317d16156b77f8537b3ba66715ddc7aacabb75
Truong Nguyen <mnguyen@evenica.com>
2024-10-11 14:47:43 -0400

    init

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,9 @@ import { getCountry } from 'countries-and-timezones'
 import { type NextRequest } from 'next/server'
 
 export default function middleware(request: NextRequest) {
-  const { country = 'CA' } = request.geo ?? {}
+  const { city = 'Toronto', country = 'CA' } = request.geo ?? {}
   const { timezones = ['Canada/Eastern'] } = getCountry(country) ?? {}
-  console.log(country, timezones)
+  console.log(country, city, timezones)
   // const time = DateTime.now().setZone('America/New_York').toISOTime({ suppressMilliseconds: true })
   // console.log(time)
 }
