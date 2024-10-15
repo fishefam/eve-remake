6cd1add13603559a007c7f852c2a94be9bf715c4
Truong Nguyen <mnguyen@evenica.com>
2024-10-15 10:57:31 -0400

    init

--- app/api/client-datetime/route.ts
+++ app/api/client-datetime/route.ts
@@ -1,6 +1,6 @@
 import { type NextRequest, NextResponse } from 'next/server'
 
-export async function GET(request: NextRequest, response: NextResponse) {
+export async function GET(request: NextRequest) {
   console.log(request.geo)
   return NextResponse.json({ r: request.geo })
 }
