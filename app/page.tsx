0383ed58c88302f70a50de743d1a800cbb57df83
Truong Nguyen <mnguyen@evenica.com>
2024-10-15 10:37:31 -0400

    init

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,5 @@ export default function Page() {
   const { __incrementalCache } = globalThis as unknown as { __incrementalCache: Record<string, string> }
   const { requestHeaders } = __incrementalCache
   console.log(requestHeaders)
-  return <>hello</>
+  return <>{JSON.stringify(requestHeaders)}</>
 }
